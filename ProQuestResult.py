a2ea0ff756dce9024e33efaf6cf0508169824720
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2019-11-02 13:47:45 -0400

    Uploading latest version of the package

---
+++ ProQuestResult.py
@@ -0,0 +1,256 @@
+import re
+import pandas as pd
+from pathlib import Path
+from bs4 import BeautifulSoup
+
+
+'''
+Optional settings:
+
+The program allows you to define two central settings below:
+
+    STOPFILES = let's you determine which file names to block when reading a directory.
+    CACHE_RAW_IN_OBJECT = let's you determine whether each ProQuestResult will contain an instance variable (_raw) that contains the raw HTML from each of the files. Turned off to save memory but turn on if you, for some reason, need to read the HTML from each of the files.
+
+'''
+
+STOPFILES = ['.DS_Store']
+CACHE_RAW_IN_OBJECT = False
+
+
+
+class ProQuestResult(object):
+    '''
+    Reads individual HTML files with ProQuest search results and provides a list of dicts (ProQuestResults.results) and DataFrame object (ProQuestResults.df) with all the details for the search results.
+    
+    Provide the script with a file variable to set it up: ProQuestResult(file). File needs to be a string or a PosixPath (see pathlib's documentation if need be).
+    
+    You can also access the search query as a string: ProQuestResults.query.
+    
+    If you request len(ProQuestResults.results) for the object, it will return the number of search results in the file.
+    
+    Note: Accessing the instance variable results and df will both generate them to order. That means that the script, depending on the number of search results in each file, can take some time to run.
+    '''
+    
+    
+    def __init__(self, file = None):
+        if file is None or (not isinstance(file, str) and not isinstance(file, Path)): raise RuntimeError(f"File ({file}) must be a string value (or a PosixPath) containing a path to a ProQuest search result file.")
+        
+        self.file = Path(file)
+        self._raw = None
+        
+        if not self.file.is_file(): raise RuntimeError(f"The provided file ({file}) needs to be a readable file.")
+        
+        with open(file) as f:
+            if CACHE_RAW_IN_OBJECT:
+                self._raw = f.read()
+                soup = BeautifulSoup(self._raw,'lxml')
+            else:
+                raw = f.read()
+                soup = BeautifulSoup(raw,'lxml')
+                
+        self.query = soup.find("textarea", {"id": "searchTerm"}).text.lstrip()
+        self._all_results = soup.find_all("li", {"class": "resultItem"})
+        self._results = None
+        self._df = None
+    
+    
+    def __len__(self):
+        return(len(self._all_results))
+    
+    
+    def __repr__(self):
+        return(f"ProQuestResult('{self.file})'")
+
+    
+    @property
+    def results(self):
+        if self._results is None: self._results = self._process_all_results()
+        return(self._results)
+    
+    
+    @property
+    def df(self):
+        if self._df is None:
+            if self._results is None: self._results = self._process_all_results()
+            
+            if self._results is None:
+                self._df = None
+            else:
+                self._df = pd.DataFrame.from_dict(self._results)
+        return(self._df)
+    
+    
+    def _process_all_results(self):
+        ''' Internal function to provide a list of dictionaries with all the search results found in the instance BeautifulSoup object _all_results. '''
+        
+        def test_regex(metadata):
+            ''' Nested function to generate a dictionary of information from regex parsed metadata about a specific article. '''
+            pattern = re.compile('(.+) \((.+)\); (.+) Vol. (.+), Iss. (.+),\s+\((.+)\): (.+).')
+            result = pattern.search(metadata)
+            if result is not None:
+                return({
+                    'publication': result[1],
+                    'vol': result[4],
+                    'issue': result[5],
+                    'date': result[6],
+                    'pages': result[7]
+                })
+            else:
+                #print("Could not decode metadata:",metadata)
+                return({
+                    'publication': None,
+                    'vol': None,
+                    'issue': None,
+                    'date': None,
+                    'pages': None
+                })
+
+        _ = []
+        for r in self._all_results:
+            # get title
+            if r.find("a", {"class": "previewTitle"}) is not None:
+                title = r.find("a", {"class": "previewTitle"}).text
+            else:
+                title = None
+
+            # get fulltext link
+            if r.find("a", {"class": "format_fulltext"}) is not None:
+                link_details = r.find("a", {"class": "format_fulltext"})
+                link_details = link_details['href']
+                link_details = link_details.split("/")
+                fulltext_link = "/"+link_details[3]+"/"+link_details[4]+"/"+link_details[5]+"/"+link_details[6]+"/"+link_details[7]+"/"+link_details[8]
+            else:
+                fulltext_link = None
+
+            # get pdf link
+            if r.find("a", {"class": "format_pdf"}) is not None:
+                link_details = r.find("a", {"class": "format_pdf"})
+                link_details = link_details['href']
+                link_details = link_details.split("/")
+                fulltext_pdf = "/"+link_details[3]+"/"+link_details[4]+"/"+link_details[5]+"/"+link_details[6]+"/"+link_details[7]+"/"+link_details[8]
+            else:
+                fulltext_pdf = None
+
+            #get all metadata
+            if r.find_all("span", {"class": "titleAuthorETC"}) is not None:
+                metadata = r.find_all("span", {"class": "titleAuthorETC"})
+                if(len(metadata) == 1):
+                    author = None
+                    metadata = metadata[0].text.replace(u'\xa0', u' ')
+                    info = test_regex(metadata)
+                elif(len(metadata) == 2):
+                    author = metadata[0].text.strip()
+                    metadata = metadata[1].text.replace(u'\xa0', u' ')
+                    info = test_regex(metadata)
+            else:
+                info = {
+                    'publication': None,
+                    'issue': None,
+                    'vol': None,
+                    'date': None,
+                    'pages': None
+                }
+                
+            _.append({
+                'author': author,
+                'publication': info['publication'],
+                'full_date': info['date'],
+                'vol': info['vol'],
+                'issue': info['issue'],
+                'title': str(title),
+                'pages': info['pages'],
+                'link_details': str(fulltext_link),
+                'link_pdf': str(fulltext_pdf),
+                'ad': " ad " in title.lower() or title[0:2].lower() == "ad" # not a great way to check this, so data points here need to be taken with a grain of salt.
+            })
+        
+        return(_)
+    
+
+class ProQuestResults(object):
+    '''
+    Flexible class that reads any number of individual HTML files with ProQuest search results and provides a list of dicts (ProQuestResults.results) and DataFrame object (ProQuestResults.df) with all the details for the search results.
+    
+    Provide the script with a list of file names as strings or PosixPaths or a directory as a string or PosixPath or a number of directories provided as a list of strings or PosixPaths to set it up:
+        ProQuestResult(files = ['list', 'of', 'paths', 'to', 'files'])
+        ProQuestResult(directory = 'the path of a directory')
+        ProQuestResult(directory = ['the path of a directory', 'the path of another directory'])
+    
+    Note: Accessing the instance variable results and df will both generate them to order. That means that the script, depending on the number of search results in each file, can take some time to run.
+    '''
+    
+    
+    def __init__(self, files=None, directory=None):
+
+        # First, let's sort out the files and directory information and generate a files instance variable (self.files) that contains a list of all the Path objects
+        if files is None and directory is not None:
+            if isinstance(directory, str) or isinstance(directory, Path):
+                # directory is a string or a Path
+                directory = Path(directory)
+                files = [x for x in Path(directory).iterdir() if x.is_file() and x.name not in STOPFILES]
+                
+            elif isinstance(directory, list) and (all(isinstance(x, str) for x in directory) or all(isinstance(x, Path) for x in directory)):
+                # directory is a list of strings or paths
+                directory = [Path(x) for x in directory]
+                files = []
+                for _dir in directory:
+                    files.extend([x for x in Path(_dir).iterdir() if x.is_file() and x.name not in STOPFILES])
+            elif isinstance(directory, list) and not (all(isinstance(x, str) for x in directory) or all(isinstance(x, Path) for x in directory)):
+                raise RuntimeError("Cannot interpret list of directories. Make sure that the list contains only strings or PosixPath objects.")
+            else:
+                raise RuntimeError("Cannot interpret list of directories. Make sure that the list contains only strings or PosixPath objects.")
+        elif files is not None:
+            if isinstance(files, list) and directory is None and all(isinstance(x, str) for x in files) or all(isinstance(x, Path) for x in files):
+                pass # we have files and no directory, so all is good
+            
+        
+        self.files = [Path(x) for x in files]
+        self.directory = directory # make sure we can access this later
+        self._results = None
+        self._df = None
+    
+    
+    def __len__(self):
+        if self._results is None: self._results = self._process_all_results()
+        return(len(self._results))
+    
+    
+    def __repr__(self):
+        return(f"ProQuestResult('{self.file})'")
+    
+    
+    @property
+    def results(self):
+        if self._results is None: self._results = self._process_all_results()
+        return(self._results)
+
+    
+    @property
+    def df(self):
+        if self._df is None:
+            if self._results is None: self._results = self._process_all_results()
+            if self._results is None:
+                self._df = None
+            else:
+                self._df = pd.DataFrame.from_dict(self._results)
+        return(self._df)
+    
+    
+    def _process_all_results(self):
+        ''' Internal function to create a list of results from each of the files, consolidated into one single list which is returned to the caller. '''
+        _ = []
+        for file in self.files:
+            results = ProQuestResult(file).results
+            _.extend(results)
+        return(_)
+    
+    
+    @property
+    def queries_to_files(self):
+        raise NotImplementedError("This feature is not yet available.")
+    
+    
+    @property
+    def files_to_queries(self):
+        raise NotImplementedError("This feature is not yet available.")
--- README.md
+++ README.md
@@ -1,13 +1,107 @@
-# process-entertainment-archive
+# Process ProQuest Entertainment Archive files
+
 A python script to traverse through HTML files with ProQuest results to generate an easily navigable CSV file (and Pandas DataFrame).
 
-# Instructions for use
-On line 36-39, define your settings:
 
-``search_result_directory`` should be set to the folder that contains the HTML files with search results from ProQuest's Entertainment Archive (automatically set to **search_results**).
+## How to Install
 
-``stopfiles`` is a list containing the files you would like to _not_ include in the creation of the CSV file (for instance, Mac OS X's really annoying **.DS_Store** file).
+This package requires you to install two other packages for it to run: `pandas` and `BeautifulSoup`. Install them by running these two commands in your command line:
 
-``savefile_csv`` should be set to the name of your results file. (automatically set to **results.csv**; but note that the script automatically adds the current date and time on line 127).
+`pip install pandas`
 
-``remove_duplicates`` should be set to **True** if you want the script to automatically remove any duplicate entries at the end of processing.
+`pip install beautifulsoup4`
+
+
+## Set Up the Program
+
+The program allows you to define two optional settings. Open `ProQuestResult.py` and find the two lines that contain the two variables `STOPFILES` and `CACHE_RAW_IN_OBJECT`.
+
+`STOPFILES` needs to be a list of strings. It determines which file names the program will block when reading a directory. By default it is set to only include one element, Mac OS X's annoyingly present .DS_Store files: `['.DS_Store']`
+
+`CACHE_RAW_IN_OBJECT` needs to be a boolean. It determines whether each ProQuestResult will contain an instance variable (`ProQuestResult._raw`) that contains the raw HTML from each of the files. By default, this variable is set to `False` in order to save memory. Switch to `True` if you for some reason need to be able to access the HTML from your search result file.
+
+
+## How to Run
+
+Drop the `ProQuestResult.py` file into your project folder. Then run `from ProQuestResult import *` in your project, whether it is a Python file or a Jupyter Notebook.
+
+You have two options when creating an object containing your search results: `ProQuestResult` (1) and `ProQuestResults` (2). The subtle difference is in the plural.
+
+
+### (1) ProQuestResult
+
+If you have one individual HTML files with ProQuest search results, this is the object you want to invoke. It provides a list of dictionaries (`ProQuestResults.results`) and a DataFrame object (`ProQuestResults.df`) with all the details for the search results.
+
+
+#### Setting up the object
+
+To set up an object, simply provide it with a file variable to set it up: 
+
+`parsed_results = ProQuestResult(file = './my_search_results/the_file_with_results.html')`
+
+The `file` parameter should be a string but can also be a PosixPath (see pathlib's documentation for reference).
+
+
+#### Accessing search results
+
+Once the object has been set up, you can easily access the search results as a list of dictionaries:
+
+`print(parsed_results.results)`
+
+If you'd rather see the search results as a pandas DataFrame, you can do so by calling:
+
+`parsed_results.df`
+
+This also provides an easy way to export the DataFrame to a CSV, by calling:
+
+`parsed_results.df.to_csv('xxx.csv')` **<-- I need to look into whether this actually works**
+
+*Note: Accessing the instance variables `results` and `df` will both generate them to order. That means that the script, depending on the number of search results in each file, can take some time to run.*
+
+The object also gives you easy access to the search query as a string:
+
+`print(parsed_results.query)`
+
+If you request `len()` for the object, it will return the number of search results in the file:
+
+`len(parsed_results)`
+
+
+### (2) ProQuestResults
+
+If you have a directory or a list of files containing search results from ProQuest and you want to collect all of them in one object, you can do so by calling `ProQuestResults` instead of the examples above.
+
+
+#### Setting up the object
+
+The program is flexible and can ingest a number of variations through the two variables it accepts: `files` or `directory`.
+
+**`files`** needs to be provided as a list of file names as strings (or PosixPaths). For example:
+
+`ProQuestResult(files = ['./first_file.html', './second_file.html', './third_file.html', './fourth_file.html'])`
+
+**`directory`** can be provided as either (1) a string (or a PosixPath) with a path to a directory containing the search result files you want to work with, or (2) a list of strings (or PosixPaths) that refer to any number of directories containing search result files.
+
+For example, if you work with a single directory (1 above), you would call:
+
+`parsed_results = ProQuestResults(directory = './my_search_results/')`
+
+If you have a number of directories you need to summarize in one object (see 2 above), you would call the same object but set it up with a list of directories:
+
+`parsed_results = ProQuestResults(directory = ['./my_first_search_result_directory/', './my_second_search_result_directory/'])`
+
+
+#### Accessing search results
+
+Once the object has been set up, you can easily access the search results in the same manner as the examples under `ProQuestResult` above:
+
+To access all the search results as a list of dictionaries: `print(parsed_results.results)`
+
+To access all the search results as a DataFrame: `parsed_results.df`
+
+*Note: As is the case with `ProQuestResult`, accessing the instance variable `results` and `df` will both generate them to order. That means that the script, depending on the number of search results in each file, can take some time to run.*
+
+
+#### Future features
+
+*Note that since the object is set up by numerous files, currently the `ProQuestResults` object cannot provide the search queries. In a future version, it will be able to provide the search query for each file (through requesting `ProQuestResults.files_to_queries`) and each file that contains each search query (through requesting `ProQuestResults.queries_to_files`).*
